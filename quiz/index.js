815a8982cd1365ebb4e55841a2a90a4d4b45e721
pedro <pedrocaetano90@gmail.com>
2024-08-22 17:23:36 -0600

    Added a simple quiz project we did as a group

---
+++ quiz/index.js
@@ -0,0 +1,8 @@
+/**
+ * Quizz on how to start a very simple node project :)
+ *  - To install a new dependency, for example, "mathjs", run `npm install mathjs` in your terminal
+ */
+
+const myFile = require('./myFile.js');
+
+console.log("Hello WOrld!" + myFile);
